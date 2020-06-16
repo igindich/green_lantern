62359fb13247e7455b07525f7cda4b608de13731
Igor Gindich <igor@foilcom.net>
2020-06-16 11:08:42 +0300

    add all request files

---
+++ lantern/flask_practics/manager.py
@@ -0,0 +1,13 @@
+from app import app.run_app
+from app.db import db
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
+
+app = run_app()
+
+migrate = Migrate(app, db)
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
+
+if __name__ == '__main__':
+    manager.run()
---
+++ lantern/flask_practics/run.py
@@ -0,0 +1,6 @@
+from app.app import run_app
+
+app = run_app()
+
+if __name__ == '__main__':
+    app.run(host ='0.0.0.0', port = 8000)
